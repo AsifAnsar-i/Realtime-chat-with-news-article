c4e387a0520bc50656657bf673d73dbe5f5abcfc
MOHD ASIF ANSARI <mohdasifansair1999@gmail.com>
2024-08-02 21:28:04 +0530

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://realtimechat90.netlify.app/",
     credentials: true,
   },
 });
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://realtimechat90.netlify.app/",
     credentials: true,
   })
 );
