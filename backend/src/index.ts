f7d7eaa2bd6c71b92bffbdde2115e547a8269901
MOHD ASIF ANSARI <mohdasifansair1999@gmail.com>
2024-08-02 20:55:47 +0530

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,6 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   },
 });
@@ -32,7 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
@@ -43,5 +41,5 @@ app.use("/api/message", messageRoutes);
 app.use("/api/news", newsRoutes);
 
 server.listen(7000, () => {
-  console.log(`Server running on port ${7000}`);
+  console.log(`Server running, on port ${7000}`);
 });
